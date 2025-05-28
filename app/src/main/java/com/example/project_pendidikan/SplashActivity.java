9cbaf725ca7cee441791705dfddb7b11d0790f75
Your Name <your.email@example.com>
2025-05-28 11:39:29 +0700

    final 1

--- app/src/main/java/com/example/project_pendidikan/SplashActivity.java
+++ app/src/main/java/com/example/project_pendidikan/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -27,7 +28,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 checkLoginStatus();
